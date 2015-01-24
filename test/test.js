bceb00313265ba68cf9aa5710d31e48cd89954c4
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-01-25 01:57:47 +0200

    improve test

--- test/test.js
+++ test/test.js
@@ -10,5 +10,12 @@ describe('network calculator', function () {
     assert.equal(nc('192.168.1.4', '255.255.255.128').firsthost, '192.168.1.1');
     assert.equal(nc('192.168.1.4', '255.255.255.128').lasthost, '192.168.1.126');
     assert.equal(nc('192.168.1.4', '255.255.255.128').totalhost, 126);
+
+    assert.equal(nc('10.1.1.1', '255.0.0.0').network, '10.0.0.0');
+    assert.equal(nc('10.1.1.1', '255.0.0.0').broadcast, '10.255.255.255');
+    assert.equal(nc('10.1.1.1', '255.0.0.0').firsthost, '10.0.0.1');
+    assert.equal(nc('10.1.1.1', '255.0.0.0').lasthost, '10.255.255.254');
+    assert.equal(nc('10.1.1.1', '255.0.0.0').totalhost, 16777214);
+    assert.equal(nc('10.1.1.1', '255.0.0.0').bitmask, 8);
   });
 });
